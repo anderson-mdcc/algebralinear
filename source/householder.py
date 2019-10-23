a26fb690dfe2da73bff4d6c14d050535b4e44f43
Anderson Calixto <andersonbr@gmail.com>
2019-10-23 16:20:56 -0300

    source

--- source/householder.py
+++ source/householder.py
@@ -1,6 +1,6 @@
 # imports
 import numpy as np
-from numpy import dot,diagonal,outer,identity,matmul,transpose,zeros_like
+from numpy import dot,diagonal,outer,identity,matmul,transpose,zeros_like,sign
 from math import sqrt
 
 norma = lambda x : np.sqrt(np.sum(np.square(x)))
@@ -10,55 +10,53 @@ def conv_col(x):
     x.shape = (1, x.shape[0])
     return x
 
-# matriz HH para vetor v
-def HH(v):
-    tam_de_v = v.shape[1]
-    e1 = zeros_like(v)
-    e1[0, 0] = 1
-    vet = norma(v) * e1
-    # funcao sinal (sign)
-    if v[0, 0] < 0:
-        vet = - vet
-    u = (v + vet).astype(np.float32)
-    m_identid = identity(tam_de_v)
-    H = m_identid - ((2 * (u.T.dot(u))) / (u.dot(u.T)))
-    return H
+def step_householder(A):
+    ai = conv_col(A[:,0]).T
+    tam_vet = ai.shape[0]
+    n_ai = norma(ai)
+    ei = zeros_like(ai)
+    ei[0,0] = 1
+    sign_a11 = sign(ai[0,0])
+    vi = ai - (sign_a11 * n_ai * ei)
+    Hi = identity(tam_vet) - 2 * ( (vi.dot(vi.T)) / (vi.T.dot(vi)) )
+    return Hi
 
-def it_qr_hh(q, r, iter, n):
-    v = conv_col(r[iter:, iter])
-    Hb = HH(v)
-    H = identity(n)
-    H[iter:, iter:] = Hb
-    r = matmul(H, r)
-    q = matmul(q, H)
+def checkDebug(flag, *args):
+    if (flag):
+        for arg in args:
+            print(arg, end = '')
+        print()
+
+def qr_householder(A, debug=False):
+    l, c = A.shape
+    its = min(l,c)
+
+    # aumenta uma linha e coluna na matriz
+    # somente para manter o mesmo procedimento no laco
+    Ai = identity(its + 1)
+    Ai[1:,1:] = A
+
+    # valor inicial de Q e R
+    q = identity(its)
+    r = A
+    checkDebug(debug, '*******************************************************')
+    for i in range(its - 1):
+        checkDebug(debug, 'Iteração ', i, ': ***********************')
+        # retira primeira linha e primeira coluna de Ai
+        Ai = Ai[1:, 1:]
+        checkDebug(debug, 'Ai (Entrada do step HouseHolder):\n', Ai)
+        Hi = step_householder(Ai)
+        Ai = Hi.dot(Ai)
+        checkDebug(debug, 'Ai para proxima iteracao (Hi atual * Ai atual):\n', Ai)
+
+        # coloca resultado Hi em Identidade para manter dimensao
+        Ii = identity(its)
+        Ii[i:,i:] = Hi
+
+        checkDebug(debug, 'Ii (Saida do step_householder [Hi] com identidade na dimensao original de A):\n', Ii)
+        r = matmul(Ii, r)
+        q = matmul(q, Ii)
+        checkDebug(debug, 'r:\n', r)
+        checkDebug(debug, 'q:\n', q)
+    checkDebug(debug, '***********************')
     return q, r
-
-A = np.matrix("3 7 8 9 12;5 -7 4 -7 8;1 1 -1 1 -1;4 3 2 1 7;9 3 2 5 4")
-l, c = np.shape(A)
-
-
-Q = identity(l)
-R = A.astype(np.float32)
-for i in range(min(l, c)):
-    Q, R = it_qr_hh(Q, R, i, l)
-
-np.set_printoptions(suppress=True)
-#print('Matriz Q:\n', Q)
-#print('Matriz R:\n', np.around(R, decimals=10))
-#print('Validação:\n', np.allclose(A, np.dot(Q, R)))
-#print('Matriz Q * R:\n', np.dot(Q, R))
-
-
-
-#np.allclose(A, np.dot(q, r))
-Qa = identity(A.shape[0])
-print('Qa ini:\n', Qa)
-ai = A
-for i in range(1000):
-    q, r = np.linalg.qr(ai)
-    qt = transpose(q)
-    ai = r * q
-    Qa = Qa * q
-print('Qa:\n', Qa)
-print('q:\n', q)
-print('r:\n', r)
